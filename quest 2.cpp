723ec17faeec53190e9a3bb02d72b03f17380a43
Liah Souza <115299056+thalia-araujoo@users.noreply.github.com>
2022-11-10 14:52:08 -0300

    Add files via upload

---
+++ quest 2.cpp
@@ -0,0 +1,14 @@
+#include <stdio.h>
+int main(){
+	int x; //declara a variavel antes tbm//
+//como são multiplos de 5, tem que pensar: 5*5=25 entt põe um 0. que fica igual á 250//
+	
+	for(int x=1; x<=250; x++) {
+		
+		if(x%5==0){
+			
+	printf("%d\n",x);
+}
+	}
+	return 0;
+}
